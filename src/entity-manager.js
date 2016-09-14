703e9156a7eb2e7d5ae1c4e3d7f11ee1d654a0de
Cody Baldwin <cbald24@win.cs.ksu.edu>
2016-09-14 15:30:28 -0500

    today's class work

---
+++ src/entity-manager.js
@@ -0,0 +1,30 @@
+module.exports = exports = EntityManager;
+
+function EntityManager(width, height, cellSize)
+{
+    this.worldWidth = width;
+    this.worldHeight = height;
+    this.cellSize = cellSize;
+    this.widthInCells = Math.ceil(width / cellSize);
+    this.numberOfCells = this.widthInCells * Math.ceil(height / cellSize);
+    this.cells = [];
+    for (var i = 0; i < this.numberOfCells; i++)
+    {
+        this.cells[i] = [];
+    }
+}
+
+EntityManager.prototype.addEntity = function(entity){
+    var left = Math.floor(entity.x / this.cellSize);
+    var right = Math.ceil((entity.x+width) / cellSize);
+    var top = Math.floor(entity.y / this.cellSize);
+    var bottom = Math.ceil((entity.y + height) / this.cellSize);
+    for(var x = left; x < right; x++) {
+        for (var y = top; y< bottom; y++)
+        {
+            this.cells[y * this.widthInCells + x].push(entity);
+            if(!entity.cells) entity.cells = [];
+            entity.cells.push({x: x, y: y});
+        }
+    }
+}
